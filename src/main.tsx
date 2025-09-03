bff06662e56b00638fbcdc27181bff8bbf78ffb1
CamelJohn <jonathan23986@gmail.com>
2025-09-03 21:39:34 +0300

    setup sitemaps and robots.txt

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+// import App from './App.tsx';
+import { HelmetProvider } from 'react-helmet-async';
+// import TextPost from './posts/test.md'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HelmetProvider>
+      {/* <Helmet> */}
+        {/* <TextPost /> */}
+      {/* </Helmet> */}
+    </HelmetProvider>
+  </StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import markdown, { Mode } from 'vite-plugin-markdown';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import VitePluginReactMarkdown from 'vite-plugin-react-markdown';
+import Pages from 'vite-plugin-pages';
+import generateSitemap from 'vite-plugin-pages-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), markdown({
-    mode: [Mode.REACT]
-  })],
+  plugins: [
+    react(),
+    VitePluginReactMarkdown(),
+    Pages({
+      onRoutesGenerated: (routes) =>
+        generateSitemap({
+          routes,
+          dest: 'public/sitemaps',
+          filename: 'sitemap.xml',
+          changefreq: 'daily',
+          priority: 0.9,
+          allowRobots: true,
+          // exclude: ['/admin/*', '/login'],
+        }),
+    }),
+  ],
   server: {
     port: 8080,
-    open: true
-  }
-})
+    open: true,
+  },
+});
