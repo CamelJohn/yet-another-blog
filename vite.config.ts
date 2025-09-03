54840bc369b38de1fd5c83e1aa4ae063bbf876fa
CamelJohn <jonathan23986@gmail.com>
2025-09-03 21:06:11 +0300

    add vite markdown plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import markdown, { Mode } from 'vite-plugin-markdown';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), markdown({
+    mode: [Mode.REACT]
+  })],
   server: {
     port: 8080,
     open: true
